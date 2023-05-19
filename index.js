403a32ae14e1a983642a6041f5b6b96136bee91e
sanaullahsabere <you@example.com>
2023-05-19 14:28:23 +0600

    react tab route done

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS);
 
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtxhup0.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,6 +27,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const carsCollection = client.db("toyCar").collection("toycars");
+
+    // react tab route is here
+    app.get("/toycars/:text", async (req, res) => {
+      if (req.params.text == "sports" || req.params.text == "hyper" || req.params.text == "luxury") {
+        const result = await carsCollection.find({ status: req.params.text }).toArray();
+        return res.send(result)
+      }
+      const result = await carsCollection.find({}).toArray();
+      res.send(result)
+      
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +49,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
